c8c6c629547e7509d452fe4476e5ca507339172a
DRTX2 <davidmanjarres2004@gmail.com>
2025-03-24 20:18:13 -0500

    My first project

---
+++ MiPrimerProyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World"); 
+        Console.WriteLine('C'); // this is a character
+        // to write numbers by default we use a double data type, as well we have float(f) and decimal(m), E.G. 0.35f
+        string firstName = "John";
+    }
+}
